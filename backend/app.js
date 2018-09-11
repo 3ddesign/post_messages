d5778cef2db4917405993579e9d6b2c9d7b0846a
3ddesign <3ddesign@bk.ru>
2018-09-11 07:43:21 +0300

    add body parser, headers

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,46 @@
-const express = require("express");
-
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use("/api/posts", (req, res, next) => {
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added succesfully'
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
   const posts = [
     {
-      id: "wfeerwwe23",
-      title: "First post",
-      content: "Thist is test first post"
+      id: 'wfeerwwe23',
+      title: 'First post',
+      content: 'Thist is test first post'
     },
     {
-      id: "wfe4234s23",
-      title: "Second post",
-      content: "Thist is test second post"
+      id: 'wfe4234s23',
+      title: 'Second post',
+      content: 'Thist is test second post'
     }
   ];
   res.status(200).json({
-    message: "Post fetched succesfully!",
+    message: 'Post fetched succesfully!',
     posts: posts
   });
 });
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -12,7 +12,9 @@ export class PostsService {
 
   getPosts() {
     this.http
-      .get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts')
+      .get<{ message: string; posts: Post[] }>(
+        'http://localhost:3000/api/posts'
+      )
       .subscribe(postData => {
         this.posts = postData.posts;
         this.postsUpdated.next([...this.posts]);
@@ -25,6 +27,11 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
+    this.http
+      .post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .subscribe(responseData => {
+        console.log(responseData.message);
+      });
     this.posts.push(post);
     this.postsUpdated.next([...this.posts]);
   }
