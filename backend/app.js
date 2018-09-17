c9a791b69cefcb87b626b94c32e558a543e78c78
3ddesign <3ddesign@bk.ru>
2018-09-17 07:49:01 +0300

    add file upload

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path')
 
-const Post = require('./models/post');
+const postsRoutes = require('./routes/posts');
 
 const app = express();
 
@@ -11,14 +12,15 @@ mongoose
     'mongodb+srv://root:s5eVDNzDmVAMPouX@test-api-mhcni.mongodb.net/node-angular?retryWrites=true'
   )
   .then(() => {
-    console.log('Connected to datebase');
+    console.log('Connected to database!');
   })
   .catch(() => {
-    console.log('Connection failed');
+    console.log('Connection failed!');
   });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/images', express.static(path.join('backend/images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -28,39 +30,11 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET, POST, PATCH, DELETE, OPTIONS'
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
   );
   next();
 });
 
-app.post('/api/posts', (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  // console.log(post);
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: 'Post added succesfully',
-      postId: createdPost._id
-    });
-  });
-});
-
-app.get('/api/posts', (req, res, next) => {
-  Post.find().then(documents => {
-    res.status(200).json({
-      message: 'Post fetched succesfully!',
-      posts: documents
-    });
-  });
-});
-
-app.delete('/api/posts/:id', (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({ message: 'Post deleted' });
-  });
-});
+app.use('/api/posts', postsRoutes);
 
 module.exports = app;
