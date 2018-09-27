bbfb8c24c05828d435c64398d312dc57f7f8511b
3ddesign <3ddesign@bk.ru>
2018-09-27 07:46:58 +0300

    add auth for delete and edit

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -39,8 +39,6 @@ router.post('', checkAuth, multer({ storage: storage }).single('image'),
       imagePath: url + '/images/' + req.file.filename,
       creator: req.userData.userId
     });
-    // console.log(req.userData);
-    // return res.status(200).json({});
     post.save().then(createdPost => {
       res.status(201).json({
         message: 'Post added successfully',
@@ -66,9 +64,12 @@ router.put('/:id', checkAuth, multer({ storage: storage }).single('image'),
       content: req.body.content,
       imagePath: imagePath
     });
-    console.log(post);
-    Post.updateOne({ _id: req.params.id }, post).then(result => {
+    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
+      if (result.nModified > 0) {
       res.status(200).json({ message: 'Update successful!' });
+      } else {
+      res.status(401).json({ message: 'Not authurized for change this message!' });
+      }
     });
   }
 );
@@ -111,11 +112,16 @@ router.get('/:id',  checkAuth, (req, res, next) => {
   });
 });
 
-router.delete('/:id', (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(result => {
-    console.log(result);
-    res.status(200).json({ message: 'Post deleted!' });
-  });
+router.delete('/:id', checkAuth, (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(
+    result => {
+      if (result.n > 0) {
+        res.status(200).json({ message: 'Deletion successful!' });
+      } else {
+        res.status(401).json({ message: 'Not authorized!' });
+      }
+    }
+  );
 });
 
 module.exports = router;
